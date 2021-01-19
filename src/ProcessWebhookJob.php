48f46b6962c45f9683ce86db4e2724d076bc6fe5
Dan Febra <dan@brainbook.com.au>
2021-01-19 17:32:17 +0800

    model

--- src/ProcessWebhookJob.php
+++ src/ProcessWebhookJob.php
@@ -13,7 +13,7 @@ abstract class ProcessWebhookJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $webhookCall = new WebhookCall();
+    public $webhookCall;
 
     public function __construct(WebhookCall $webhookCall)
     {
